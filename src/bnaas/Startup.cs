bbbfea4a8d52042ab89a1581878c96fe0b8489ea
Russell Dear <russell.dear@gmail.com>
2018-12-03 07:11:13 +1300

    5

--- src/bnaas/Startup.cs
+++ src/bnaas/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace bnaas
 {
@@ -18,6 +19,11 @@ namespace bnaas
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Band Names As A Service", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -32,6 +38,14 @@ namespace bnaas
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Band Names As A Service V1");
+            });
+
             app.UseMvc();
         }
     }
